144b30869fde19feda810ad4eea146698544dbd3
RaisaRamazanova <ramazanova.raisat@yandex.ru>
2021-08-18 00:16:49 +0300

    Update Contents.swift

--- Лекция05/SumOfStrings.playground/Contents.swift
+++ Лекция05/SumOfStrings.playground/Contents.swift
@@ -1,15 +1,85 @@
 import UIKit
 
-var number1 = "132"
-var number2 = "40"
-
-func sum(num1: String, num2: String) -> String {
-    return String(Int(num1)! + Int(num2)!)
+func pow(_ x: Int, _ y: Int) -> Int {
+  var result = 1
+    for _ in 0..<y {
+    result *= x
+  }
+  return result
 }
 
-var summa = sum(num1: number1, num2: number2)
 
-print(sum(num1: "0", num2: "99807654321") == "99807654321")
+func sum(num1: String, num2: String) -> String {
+    var i = 0
+    var summa = ""
+    var n1 = num1
+    var n2 = num2
+    var count = 0
+    var flag = 0
+    if num1.count < num2.count {
+        count = num2.count
+    } else {count = num1.count}
+    
+    while i < num1.count && i < num2.count {
+        var timeSum = 0
+        timeSum = Int(String(n1[n1.index(before: n1.endIndex)]))! + Int(String(n2[n2.index(before: n2.endIndex)]))!
+        print("timeSum = ", timeSum)
+        if flag == 1 {
+            timeSum += 1
+            flag = 0
+        }
+        if timeSum > 9 {
+            timeSum = timeSum - 10
+            flag = 1
+        }
+        
+        summa.append(String(timeSum))
+        n1.remove(at: n1.index(before: n1.endIndex))
+        n2.remove(at: n2.index(before: n2.endIndex))
+        i += 1
+    }
+    if n1 == "" {
+        while i < count {
+            var timeSum = Int(String(n2[n2.index(before: n2.endIndex)]))!
+            if flag == 1 {
+                timeSum += 1
+                flag = 0
+            }
+            if timeSum > 9 {
+                timeSum = timeSum - 10
+                flag = 1
+            }
+            summa.append(String(timeSum))
+            n2.remove(at: n2.index(before: n2.endIndex))
+            i += 1
+        }
+    }
+    if n2 == "" {
+        while i < count {
+            var timeSum = Int(String(n1[n1.index(before: n1.endIndex)]))!
+            if flag == 1 {
+                timeSum += 1
+                flag = 0
+            }
+            if timeSum > 9 {
+                timeSum = timeSum - 10
+                flag = 1
+            }
+            summa.append(String(timeSum))
+            n1.remove(at: n1.index(before: n1.endIndex))
+            i += 1
+        }
+    }
+    
+    if flag == 1 {
+        summa.append(String(1))
+    }
+    let str = String(summa.reversed())
+    return str
+}
+
+
+print(sum(num1: "123456", num2: "123") == "123579")
 print(sum(num1: String(Int.max), num2: String(Int.max)) == "18446744073709551614")
 print(sum(num1: "0", num2: "0") == "0")
 
