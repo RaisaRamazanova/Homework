8332e97507454e919587d5eefd1a9faa29c5e8ba
RaisaRamazanova <ramazanova.raisat@yandex.ru>
2021-08-17 22:31:48 +0300

    Update Contents.swift

--- Лекция05/SumOfStrings.playground/Contents.swift
+++ Лекция05/SumOfStrings.playground/Contents.swift
@@ -3,8 +3,17 @@ import UIKit
 var number1 = "132"
 var number2 = "40"
 
-func sum(number1: String, number2: String) -> String {
-    return String(Int(number1)! + Int(number2)!)
+func sum(num1: String, num2: String) -> String {
+    return String(Int(num1)! + Int(num2)!)
 }
 
-var summa = sum(number1: number1, number2: number2)
+var summa = sum(num1: number1, num2: number2)
+
+print(sum(num1: "0", num2: "99807654321") == "99807654321")
+print(sum(num1: String(Int.max), num2: String(Int.max)) == "18446744073709551614")
+print(sum(num1: "0", num2: "0") == "0")
+
+print(sum(num1: "98765", num2: "5") == "98770")
+
+print(sum(num1: "98765", num2: "495") == "99260")
+print(sum(num1: "1126351526125351621621261", num2: "123212136263623127182182818218212121212") == "123212136263624253533708943569833742473")
